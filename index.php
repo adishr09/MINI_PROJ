2f14ffe702b0ee0bd50b3fb0a3287f1cc7a214de
Aditya Dev Sharma <adi.shr09@gmail.com>
2015-02-17 22:52:28 +0530

    Initial design

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>Aristdie - CMS </title>
+	<link rel="stylesheet" href="css/body.css" type="text/css">
+</head>
+<body>
+	<div class="top_bar">
+	<p> Login Status</p>
+	</div>
+	<div class="logo_bar">
+	<p> LOGO </p>
+	</div>
+	<div class="nav_bar">
+	<p> Nav Bar </p>
+	</div>
+	<div class="content_lol">
+		<?php include("author/index.php");?> 
+	</div>
+</body>
+</html>
