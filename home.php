8b10fb5d652e71eb255058de589e82c6b9d5be7c
Aditya Dev Sharma <adi.shr09@gmail.com>
2015-03-03 22:29:27 +0530

    TYAGI UPDATES

--- home.php
+++ home.php
@@ -4,7 +4,8 @@
 	<link rel="stylesheet" href="css/body.css" type="text/css">
 </head>
 <body>
-	<div class="top_bar">
+	
+ <div class="top_bar">
 	<button onclick="overlay()" type="button"> Hello</button>
 	<p>LogIn  /  SignUp</p>
 	</div>
@@ -16,6 +17,59 @@
 	</div>
 	<div class="content_lol">
 		<div id="overlay"><p>Hello</p></div>
+	
+      <form id='register' action='register.php' method='post' accept-charset='UTF-8'>
+      <fieldset>
+      <legend>Register</legend>
+      <input type='hidden' name='submitted' id='submitted' value='1'/>
+      
+	  <label>
+        Full Name  *
+        <input type='text' name='name' id='name' maxlength="50" /> 
+      </label>
+	   
+	  <label>
+	   Email Address  *
+        <input type='text' name='email' id='email' maxlength="50" /> 
+      </label>
+      
+	  <label>
+	   Contact No.*
+        <input type='text' name='username' id='username' maxlength="50" /> 
+      </label> 
+      
+	  <label>
+	    Subject Category*:
+         <select>
+         <option value="Sub1">s1</option>
+         <option value="sub2">s2</option>
+         <option value="sub3">s3</option>
+         <option value="sub4">s4</option>
+          </select>
+      </label>
+	  
+	  <label>
+	    Paper Type*:
+         <input type='password' name='password' id='password' maxlength="50" /> 
+      </label>
+
+	  
+	  <label> 
+	   Gender*:      
+	    <input type="radio" name="sex" value="male"> M
+        <input type="radio" name="sex" value="female"> F
+	  </label>
+	  
+	  <label>
+	    Abstract*:
+         <textarea rows="4" cols="50">
+          </textarea>		 
+      </label>
+
+	  
+	  <input type='submit' name='Submit' value='Submit' />
+      </fieldset>
+      </form>	
 	</div>
 	<script>
 	function overlay(){
