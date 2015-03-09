4ce2c21a62a74d3c3135ae48e5761c5fcd6c4c2c
Aditya Dev Sharma <adi.shr09@gmail.com>
2015-03-10 02:53:15 +0530

    content in nav bar

--- Nav_Bar/nav_bar.php
+++ Nav_Bar/nav_bar.php
@@ -4,44 +4,30 @@
 		<center>
 		<ul id="nav1">
 			<li><a href="index.html">Home</a></li>
-			<li><a href="#">About JMDET</a>
+			<li><a href="#">About ARISTIDE</a>
 				<ul>
 					<li><a href="mission_vision.html">MISSION & VISION</a></li>
 					<li><a href="editorial_board.html">EDITORIAL BOARD</a></li>
 					<li><a href="advisory_board.html">ADVISORY BOARD</a></li>
 				</ul>
 			</li>
-			<li><a href="#">Call For Papers</a>
+			<li><a href="#">INSTALLATION</a>
 				<ul>
 					<li><a href="call_for_papers.html">LEAFLET</a></li>
 					<li><a href="current_issue.html">CURRENT ISSUE</a></li>
 					<li><a href="submit_manuscript_c.html">SUBMIT MANUSCRIPT</a></li>
 				</ul>
 			</li>
-			<li><a href="#">Authors</a>
-				<ul>
-					<li><a href="submit_manuscript_a.html">SUBMIT MANUSCRIPT</a></li>
-					<li><a href="authors_guidelines.html">AUTHOR\'S GUIDELINES</a></li>
-					<li><a href="get_hard_copy.html">GET HARD COPY</a></li>
-					<li><a href="faqs.html">FAQs</a></li>
-				</ul>
+			<li><a href="#">NEWS</a>
 			</li>
-			<li class="current"><a href="#">Downloads</a>
-				<ul>
-					 
-					<li><a href="copyright_form.html">COPYRIGHT FORM</a></li>
-					<li><a href="jmdet_template.html">JMDET TEMPLATE</a></li>
-				</ul>
+			<li class="current"><a href="#">USERS</a>
 			</li>
-			<li><a href="#">Archive</a>
+			<li><a href="#">Licenses</a>
 				
 			</li>
-			<li><a href="#">Peer Review</a>
-				<ul>
-					<li><a href="peer_review_process.html">PEER REVIEW PROCESS</a></li>
-				    <li><a href="join_as_reviewer.html">JOIN AS A REVIEWER</a></li>
-				</ul>
+			<li><a href="#">Testimonials</a>
 			</li>
+			<li><a href="contact_us.html">FAQs</a></li>
 			<li><a href="contact_us.html">Contact Us</a></li>
 		</ul>
 		</center>
