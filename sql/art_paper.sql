c12f93041c646f95669db59622f593e5289ccb75
Divyanshu Sharma <divyanshu8sharma@gmail.com>
2015-04-28 01:41:02 +0530

    Import this

--- sql/art_paper.sql
+++ sql/art_paper.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 27, 2015 at 09:46 PM
+-- Generation Time: Apr 27, 2015 at 10:10 PM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.9
 
@@ -33,15 +33,18 @@ CREATE TABLE IF NOT EXISTS `art_paper` (
   `aut_id` int(100) NOT NULL,
   `rev_id` int(100) DEFAULT NULL,
   `response_words` varchar(200) DEFAULT NULL,
-  `response_list` varchar(40) DEFAULT NULL
+  `response_list` varchar(40) DEFAULT NULL,
+  `sub_cat` varchar(20) NOT NULL,
+  `paper_type` varchar(20) NOT NULL,
+  `tags` varchar(100) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `art_paper`
 --
 
-INSERT INTO `art_paper` (`paper_id`, `paper_title`, `paper_abstract`, `aut_id`, `rev_id`, `response_words`, `response_list`) VALUES
-(0, 'hello', 'hello', 0, 0, NULL, NULL);
+INSERT INTO `art_paper` (`paper_id`, `paper_title`, `paper_abstract`, `aut_id`, `rev_id`, `response_words`, `response_list`, `sub_cat`, `paper_type`, `tags`) VALUES
+(0, 'hello', 'hello', 0, 0, NULL, NULL, '', '', NULL);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
