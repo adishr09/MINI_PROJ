def6ea45a5d3b73a5fd92e7b5bc3e700aacb0bad
Divyanshu Sharma <divyanshu8sharma@gmail.com>
2015-04-28 04:33:16 +0530

    yaes

--- response.php
+++ response.php
@@ -6,6 +6,7 @@ $paper_id=$_POST['papid'];
 $paper_type = $_POST['password'];
 $response_words = $_POST['response_words'];
 $response_opt = $_POST['response_opt'];
+$q= "UPDATE `delhibvce`.`art_paper` SET `rev_id` = '1', `response_words` = '".$response_words."', `response_list` = '".$response_opt."', `sub_cat` = '".$subject_category."', `paper_type` = '".$paper_type."', `tags` = NULL WHERE 'paper_id' = '".$paper_id."'";
 mysql_connect('localhost', 'root','') or die("<br/>error");
 mysql_select_db('delhibvce') or die("<br>DB_error");
 echo $q;
