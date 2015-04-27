05a181e0d6c45bbff2ac38b36bef42e9ef88909f
Divyanshu Sharma <divyanshu8sharma@gmail.com>
2015-04-28 03:47:55 +0530

    kk

--- sql/delhibvce.sql
+++ sql/delhibvce.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
+-- version 4.1.6
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 27, 2015 at 06:53 PM
--- Server version: 5.6.24
--- PHP Version: 5.6.8
+-- Generation Time: Apr 28, 2015 at 12:17 AM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `art_author` (
-  `aut_id` int(100) NOT NULL,
+  `aut_id` int(100) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(24) NOT NULL,
   `middle_name` varchar(20) DEFAULT NULL,
   `last_name` varchar(20) NOT NULL,
@@ -35,8 +35,19 @@ CREATE TABLE IF NOT EXISTS `art_author` (
   `pass` varchar(30) NOT NULL,
   `contact_no` int(15) NOT NULL,
   `address` varchar(200) NOT NULL,
-  `country` varchar(30) NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `country` varchar(30) NOT NULL,
+  PRIMARY KEY (`aut_id`),
+  UNIQUE KEY `email_id` (`email_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `art_author`
+--
+
+INSERT INTO `art_author` (`aut_id`, `first_name`, `middle_name`, `last_name`, `email_id`, `pass`, `contact_no`, `address`, `country`) VALUES
+(2, 'Priyansh', 'The', 'Great', 'lol@lol.com', 'b6d78e18e10fc1aa0b2f3eb068b658', 1234567890, 'lpllpllp', 'india'),
+(3, 'asd', 'fasd', 'asdfa', 'asdf@adf.com', 'a7bbc53512c0539c7fffa4033d75a2', 1234567890, 'iugiuygiu', 'iyugyu'),
+(4, 'a', 's', 's', 's', '03c7c0ace395d80182db07ae2c30f0', 0, 's', 's');
 
 -- --------------------------------------------------------
 
@@ -49,10 +60,22 @@ CREATE TABLE IF NOT EXISTS `art_paper` (
   `paper_title` varchar(100) NOT NULL,
   `paper_abstract` varchar(600) NOT NULL,
   `aut_id` int(100) NOT NULL,
-  `rev_id` int(100) NOT NULL,
+  `rev_id` int(100) DEFAULT NULL,
   `response_words` varchar(200) DEFAULT NULL,
-  `response_list` varchar(40) DEFAULT NULL
+  `response_list` varchar(40) DEFAULT NULL,
+  `sub_cat` varchar(20) NOT NULL,
+  `paper_type` varchar(20) NOT NULL,
+  `tags` varchar(100) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `art_paper`
+--
+
+INSERT INTO `art_paper` (`paper_id`, `paper_title`, `paper_abstract`, `aut_id`, `rev_id`, `response_words`, `response_list`, `sub_cat`, `paper_type`, `tags`) VALUES
+(1, 'hello', 'hello', 0, 0, NULL, NULL, 'Sub1', '', NULL),
+(1, 'hello', 'hello', 0, 0, NULL, NULL, 'Sub1', '', NULL),
+(1, 'hello', 'hello', 0, 0, NULL, NULL, 'Sub1', '', NULL);
 
 -- --------------------------------------------------------
 
@@ -70,41 +93,12 @@ CREATE TABLE IF NOT EXISTS `art_reviewer` (
   `country` varchar(30) NOT NULL,
   `specialization` varchar(25) NOT NULL,
   `no_papers` int(10) DEFAULT NULL,
-  `past_reviews` bigint(255) DEFAULT NULL,
   `contact_no` int(100) DEFAULT NULL,
-  `rev_id` int(100) NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `rev_id` int(100) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`rev_id`),
+  UNIQUE KEY `email` (`email_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `art_author`
---
-ALTER TABLE `art_author`
-  ADD PRIMARY KEY (`aut_id`), ADD UNIQUE KEY `email_id` (`email_id`);
-
---
--- Indexes for table `art_reviewer`
---
-ALTER TABLE `art_reviewer`
-  ADD PRIMARY KEY (`rev_id`), ADD UNIQUE KEY `email` (`email_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `art_author`
---
-ALTER TABLE `art_author`
-  MODIFY `aut_id` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `art_reviewer`
---
-ALTER TABLE `art_reviewer`
-  MODIFY `rev_id` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
