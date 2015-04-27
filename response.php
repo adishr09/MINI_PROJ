5fdd1b73546095c37ee7b1a30bb86ff4f734ad46
Divyanshu Sharma <divyanshu8sharma@gmail.com>
2015-04-28 03:34:12 +0530

    response

---
+++ response.php
@@ -0,0 +1,16 @@
+<?php
+
+session_start();
+$subject_category = $_POST['category'];
+$paper_id=$_POST['papid'];
+$paper_type = $_POST['password'];
+$response_words = $_POST['response_words'];
+$response_opt = $_POST['response_opt'];
+$q= "INSERT INTO `delhibvce`.`art_paper` (`rev_id`, `response_words`, `response_list`, `sub_cat`, `paper_type`, `tags`) VALUES ('1', '".$response_words."', '".$response_opt."', '".$subject_category."', '".$paper_type."', NULL) WHERE (`paper_id` = '".$paper_id."');";
+mysql_connect('localhost', 'root','') or die("<br/>error");
+mysql_select_db('delhibvce') or die("<br>DB_error");
+echo $q;
+$q_run=mysql_query($q) or die("<br/>error_run");
+$q_row=mysql_fetch_assoc($q_run);
+echo $q_row
+?>
