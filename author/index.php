3441fa05025c4fd7ccaffc153c7a8bdd8bfbc4b1
Divyanshu Sharma <divyanshu8sharma@gmail.com>
2015-04-27 13:49:00 +0530

    Bhaiii

--- author/index.php
+++ author/index.php
@@ -31,14 +31,20 @@
 		        <label for="tab-3" class="tab-label-3">Work</label>
 			
 	            <input id="tab-4" type="radio" name="radio-set" class="tab-selector-4" />
-		        <label for="tab-4" class="tab-label-4">Contact</label>
+		        <label for="tab-4" class="tab-label-4">Papers</label>
             
 			    <div class="clear-shadow"></div>
 				
 		        <div class="content">
 			        <div class="content-1">
 						<h2>Profile of the Admin</h2>
-                        
+                        <Table>
+						<tr><td><label>NAME:</label></td><td></td></tr>
+						<tr><td><label>E-MAIL:</label></td><td></td></tr>
+						<tr><td><label>ADDRESS:</label></td><td></td></tr>
+						<tr><td><label>CONTACT:</label></td><td></td></tr>
+						</TABLE>
+						<button><h4>Update Profile</h4></button>
 				    </div>
 			        <div class="content-2">
 						<h2>Submit Manuscript</h2>
@@ -82,23 +88,17 @@
 							  </td></tr></table>
 							  </form>
 					
-                           
-							  					
-					
-					
-					
 					</div>
 			        <div class="content-3">
-						<h2>Portfolio</h2>
-                        <p>The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother\'s keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee.</p>
-						<h3>Examples</h3>
-						<p>Now that we know who you are, I know who I am. I\'m not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain\'s going to be? He\'s the exact opposite of the hero. And most times they\'re friends, like you and me! I should\'ve known way back when... You know why, David? Because of the kids. They called me Mr Glass. </p>
-				    </div>
+						<h2>UPDATES</h2>
+						
+					</div>
+
 				    <div class="content-4">
-						<h2>Contact</h2>
-                        <p>You see? It\'s curious. Ted did figure it out - time travel. And when we get back, we gonna tell everyone. How it\'s possible, how it\'s done, what the dangers are. But then why fifty years in the future when the spacecraft encounters a black hole does the computer call it an \'unknown entry event\'? Why don\'t they know? If they don\'t know, that means we never told anyone. And if we never told anyone it means we never made it back. Hence we die down here. Just as a matter of deductive logic.</p>
-						<h3>Get in touch</h3>
-						<p>Well, the way they make shows is, they make one show. That show\'s called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they\'re going to make more shows. Some pilots get picked and become television programs. Some don\'t, become nothing. She starred in one of the ones that became nothing. </p>
+						<h2>VIEW SUBBMISSIONS</h2>
+                        <TABLE>
+						
+						</TABLE>
 				    </div>
 		        </div>
 			</section>
