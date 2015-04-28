a2260cc786a65254597fa5e958a21507c20f1fda
Divyanshu Sharma <divyanshu8sharma@gmail.com>
2015-04-28 12:53:00 +0530

    ;

--- login.php
+++ login.php
@@ -61,7 +61,7 @@ if(strcmp($tem,$q_row['pass']) ){
         
 if($core->send()) {
     echo 'EMail sent';
-	header( 'Location: home.php' )
+	header( 'Location: home.php' );
 }
 else {
     echo $core ->ErrorInfo;
